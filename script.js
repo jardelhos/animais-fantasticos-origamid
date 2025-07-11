8262068e966652722b4f767ea0405ef172b0e442
Jardel Oliveira <jardel-h@hotmail.com>
2025-07-11 12:00:50 -0300

    accordion list

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function initTabnav(){
+
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 tabContent[0].classList.add('ativo')
@@ -18,3 +20,30 @@ tabMenu.forEach((itemMenu,index)=>{
 })
 
 }
+}
+
+initTabnav();
+
+function initAccordion(){
+
+    const accordionList = document.querySelectorAll(".js-accordion dt");
+    const activeClass = "ativo";
+
+    if(accordionList.length){
+
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+
+
+        function activeaccordion(event){
+        this.classList.toggle(activeClass)
+        this.nextElementSibling.classList.toggle(activeClass)
+        }
+
+        accordionList.forEach((item)=>{
+        item.addEventListener('click',activeaccordion)
+        })
+    }
+}
+
+initAccordion()
