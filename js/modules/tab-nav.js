8c28932838067365da0403190a20f17847886bd1
Jardel Oliveira <jardel-h@hotmail.com>
2025-10-24 15:35:53 -0300

    modulos

---
+++ js/modules/tab-nav.js
@@ -0,0 +1,25 @@
+export default function initTabnav(){
+
+const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+const tabContent = document.querySelectorAll('[data-tab="content"] section');
+tabContent[0].classList.add('ativo')
+
+if(tabMenu.length && tabContent.length){
+
+function activeTab(index){
+    tabContent.forEach((section)=>{
+        section.classList.remove('ativo')
+    })
+    const direcao = tabContent[index].dataset.anime;
+    tabContent[index].classList.add('ativo', direcao)
+}
+
+tabMenu.forEach((itemMenu,index)=>{
+    itemMenu.addEventListener("click",function(){
+        activeTab(index);
+    })
+})
+
+}
+}
+
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initTabnav from './modules/tab-nav.js'
+
+initTabnav();
+
+import initAccordion from './modules/accordion.js'
+
+initAccordion();
+
+import initScrollSuave from './modules/scroll-suave.js'
+
+initScrollSuave();
+
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+
+initAnimacaoScroll();
