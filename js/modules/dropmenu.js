430cc24be9b2bf01050e22c93b7a3df091eab696
Jardel Oliveira <jardel-h@hotmail.com>
2025-12-15 12:08:13 -0300

    clickoutside

--- js/modules/dropmenu.js
+++ js/modules/dropmenu.js
@@ -1,20 +1,29 @@
+import outsideClick from "./outside.js"
+
+
 export default function initDropMenu(){
+    const dropdownmenus = document.querySelectorAll('[data-dropdown]')
 
 
-
-}
-
-const dropdownmenus = document.querySelectorAll('[data-dropdown]')
-
-
-dropdownmenus.forEach(menu =>{
-    ['touchstart','click'].forEach(Userevent=>{
-        menu.addEventListener(Userevent,handleClick)
+    dropdownmenus.forEach(menu =>{
+        ['touchstart','click'].forEach(Userevent=>{
+            menu.addEventListener(Userevent,handleClick)
+        })
+        
     })
-    
-})
 
-function handleClick(event){
-    event.preventDefault()
-    this.classList.toggle('active')
+    function handleClick(event){
+        event.preventDefault()
+        this.classList.add('active')
+        outsideClick(this,['touchstart','click'],() => {
+            this.classList.remove('active')
+        })
+
+
+    }
+
+
 }
+
+
+
